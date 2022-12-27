636bc42ec5ad951e7cd3ce16fda45bbd38badd0a
Alexandra Davydova <davydovaalexandra225@gmail.com>
2022-12-27 16:33:47 +0300

    added models

---
+++ models/cards.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-undef */
+const mongoose = require('mongoose');
+
+const cardSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    required: true
+  },
+  link: {
+    type: String,
+    required: true
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: '',
+    required: true
+  },
+  likes: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: '',
+    default: []
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+module.exports = mongoose.model('card', cardSchema);
