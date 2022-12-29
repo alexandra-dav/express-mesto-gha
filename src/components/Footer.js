0d3a7bdf1a7509d12945db189968ede5b90e638a
Alexandra Davydova <davydovaalexandra225@gmail.com>
2022-12-29 15:35:21 +0300

    fixed

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Footer(){
-  return (
-    <footer className="footer">
-      <p className="footer__content">&copy; 2022 Mesto Russia</p>
-    </footer>
-  );
-}
