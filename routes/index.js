90625c0a2a08959d34cd535385d8ced076cf6d2e
Alexandra Davydova <davydovaalexandra225@gmail.com>
2023-02-10 15:19:42 +0300

    added cors and logs

--- routes/index.js
+++ routes/index.js
@@ -8,11 +8,20 @@ const {
 const {
   login, createUser,
 } = require('../controllers/users');
+const { requestLogger, errorLogger } = require('../middlewares/logger');
 const NotFoundError = require('../middlewares/not-found-err');
 
 const pattern = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9._]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=[\]]*)#?$/m;
+// Массив доменов, с которых разрешены кросс-доменные запросы
+const allowedCors = [
+  'https://http://your.best.mesto.nomoredomainsclub.ru/',
+  'http://http://your.best.mesto.nomoredomainsclub.ru/',
+  'localhost:3000',
+];
 
 const router = Router();
+
+router.use(requestLogger); // подключаем логгер запросов
 
 router.use(cors());
 router.post('/signin', celebrate({
@@ -37,14 +46,17 @@ router.use('/cards', authorization, require('./cards'));
 
 router.use('*', (req, res, next) => (next(new NotFoundError(errorMassage.PAGE_NOT_FOUND))));
 
+router.use(errorLogger); // подключаем логгер ошибок
+
 router.use(errors()); // обработчик ошибок celebrate
 
 router.use((err, req, res, next) => {
   const { statusCode = statusCodeName.ERROR_INTERNAL_SERVER, message } = err;
-  /* TODO обрабатывать ошибки errors() кастомно
-  if (res.message === 'Validation failed') {
-    res.status(400).send({ message: errorMassage.CARD_ID_NOT_FOUND });
-  } */
+  const { origin } = req.headers;
+  if (allowedCors.includes(origin)) {
+    // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
+    res.header('Access-Control-Allow-Origin', origin);
+  }
   res.status(statusCode).send({ message });
   next();
 });
