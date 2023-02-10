5652c27fdbac896fe82086c8abe79779575dd1f9
Alexandra Davydova <davydovaalexandra225@gmail.com>
2023-02-10 15:20:30 +0300

    added cors and logs

---
+++ __tests__/endpoint.test.js
@@ -0,0 +1,26 @@
+/* const supertest = require('supertest');
+const { describe, it, expect } = require('jest');
+const app = require('./app');
+
+const request = supertest(app);
+
+beforeAll(() => {
+  // Подключаемся к базе данных
+});
+
+afterAll(() => {
+  // Отменяем подключение к БД
+});
+
+describe('Database tests', () => {
+
+  beforeEach(() => {
+    // Перед каждым тестом добавляем нужные данные в БД
+  });
+
+  afterEach(() => {
+    // После каждого теста убираем данные из БД
+  });
+
+  it('should do the test work...', () => {});
+}) */
